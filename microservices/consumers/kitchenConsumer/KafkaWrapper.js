dcdc34f6c3daa8f6d55c7d524843f44bd607a6d7
Anthony Amanse <ghieamanse@gmail.com>
2021-02-22 21:52:40 -0800

    Initial backend architecture
    Signed-off-by: Anthony Amanse <ghieamanse@gmail.com>

---
+++ microservices/consumers/kitchenConsumer/KafkaWrapper.js
@@ -0,0 +1,119 @@
+const Kafka = require('node-rdkafka');
+
+class KafkaWrapper {
+    constructor(ibmcloud_credentials) {
+        // ibm cloud service credentials
+        let jsonCredentials = JSON.parse(ibmcloud_credentials)
+        let brokers = jsonCredentials.kafka_brokers_sasl
+        let apiKey = jsonCredentials.api_key
+        // producer
+        let driver_options = {
+            //'debug': 'all',
+            'metadata.broker.list': brokers,
+            'security.protocol': 'SASL_SSL',
+            'sasl.mechanisms': 'PLAIN',
+            'sasl.username': 'token',
+            'sasl.password': apiKey,
+            'log.connection.close' : false,
+            'enable.auto.commit': false
+        };
+        let consumerConfig = {
+            // 'client.id': 'orderconsumer',+ UUID?
+            'group.id': 'kitchen-consumer-group',
+        }
+
+        for (var key in driver_options) {
+            consumerConfig[key] = driver_options[key]
+        }
+
+        // create kafka consumer
+        let topicConfig = {
+            'auto.offset.reset': 'earliest'
+        }
+        let consumer = new Kafka.KafkaConsumer(consumerConfig, topicConfig)
+
+        // Register error listener
+        consumer.on('event.error', function(err) {
+            console.error('Error from consumer:' + JSON.stringify(err));
+        });
+
+        this.consumer = consumer
+
+        // producer
+        let producerConfig = {
+            'client.id': 'example-orders-producer',
+            'dr_msg_cb': true  // Enable delivery reports with message payload
+        }
+
+        for (var key in driver_options) {
+            producerConfig[key] = driver_options[key]
+        }
+
+        // create kafka producer
+        let producerTopicConfig = {
+            'request.required.acks': -1
+            // 'produce.offset.report': true
+        }
+        let producer = new Kafka.Producer(producerConfig, producerTopicConfig)
+        producer.setPollInterval(100)
+
+        // debug
+        // producer.on('event.log', function(log) {
+        //     console.log(log);
+        // });
+
+        // Register error listener
+        producer.on('event.error', function(err) {
+            console.error('Error from producer:' + JSON.stringify(err));
+        });
+
+        // Register delivery report listener
+        producer.on('delivery-report', function(err, dr) {
+            if (err) {
+                console.error('Delivery report: Failed sending message ' + dr.value);
+                console.error(err);
+                // We could retry sending the message
+            } else {
+                console.log('Message produced, partition: ' + dr.partition + ' offset: ' + dr.offset);
+            }
+        });
+        producer.connect()
+
+        this.producer = producer
+    }
+  
+    on(event, callback) {
+      this.consumer.on(event, callback)
+    }
+
+    createEvent(order, event, callback) {
+        let topicName = 'orders'
+        let eventType = event
+        let message = Buffer.from(JSON.stringify({eventType, payload: order}))
+        try {
+            this.producer.produce(
+                topicName,
+                null,
+                message,
+                order.orderId
+            )
+            callback(null)
+        } catch (err) {
+            console.log('caught')
+            callback(err)
+        }
+    }
+
+    preparingFoodEvent(order, callback) {
+        this.createEvent(order, 'kitchenPreparingFood', callback)
+    }
+
+    foodReadyEvent(order, callback) {
+        this.createEvent(order, 'kitchenFoodReady', callback)
+    }
+}
+
+const kafkaWrapper = new KafkaWrapper(process.env.KAFKA_CREDENTIALS)
+Object.freeze(kafkaWrapper)
+
+module.exports = kafkaWrapper
---
+++ microservices/consumers/kitchenConsumer/app.js
@@ -0,0 +1,110 @@
+const KafkaWrapper = require('./KafkaWrapper.js')
+const mongoose = require('mongoose');
+const Redis = require('ioredis');
+const MUUID = require('uuid-mongodb').mode('relaxed');
+const Order = require('./models/order.js')
+
+// connect to redis localhost
+const redis = new Redis()
+
+// environment variables for mongodb connection
+const MONGODB_REPLICA_HOSTNAMES = process.env.MONGODB_REPLICA_HOSTNAMES
+// const MONGODB_REPLICA_SET = process.env.MONGODB_REPLICA_SET
+// const MONGODB_DBNAME = process.env.MONGODB_DBNAME
+// const MONGODB_AUTH_DBNAME = process.env.MONGODB_AUTH_DBNAME || MONGODB_DBNAME
+// const MONGODB_CA_PATH = process.env.MONGODB_CA_PATH
+// const MONGODB_USER = process.env.MONGODB_USER
+// const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD
+mongoose
+    .connect('mongodb://' + MONGODB_REPLICA_HOSTNAMES + '/', {
+        // user: MONGODB_USER,
+        // pass: MONGODB_PASSWORD,
+        dbName: "example-food-delivery",
+        // replicaSet: MONGODB_REPLICA_SET,
+        // authSource: MONGODB_AUTH_DBNAME,
+        // tls: true,
+        // tlsCAFile: MONGODB_CA_PATH,
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true})
+    .catch(error => {
+        console.log(error)
+        process.exit(1)
+    });
+
+KafkaWrapper.consumer.on('ready', function() {
+    console.log('The consumer has connected.');
+    KafkaWrapper.consumer.subscribe(['orders']);
+    KafkaWrapper.consumer.consume()
+}).on('data', function(data) {
+    try {
+        let dataObject = JSON.parse(data.value.toString())
+        // dataObject
+        // {eventType, payload: {orderId,userId,kitchenId,requestId}}
+        let eventType = dataObject.eventType
+        let order = dataObject.payload
+        switch (eventType) {
+            case "courierMatched":
+                // simulate to 5 seconds process
+                setTimeout(() => {
+                    KafkaWrapper.preparingFoodEvent(order, err => {
+                        if (err) {
+                            console.log("error producing event")
+                            console.error(err)
+                        } else {
+                            console.log(`kitchenPreparingFood event for ${order.orderId} created`)
+                        }
+                    })
+                }, 5000)
+                KafkaWrapper.consumer.commitMessage(data)
+                break;
+            case "kitchenPreparingFood":
+                // simulate to 5 seconds process
+                setTimeout(() => {
+                    KafkaWrapper.foodReadyEvent(order, err => {
+                        if (err) {
+                            console.log("error producing event")
+                            console.error(err)
+                        } else {
+                            console.log(`kitchenFoodReady event for ${order.orderId} created`)
+                        }
+                    })
+                }, 5000)
+                KafkaWrapper.consumer.commitMessage(data)
+                break;
+            default:
+                console.log(`${dataObject.eventType} is not handled in this service`)
+                KafkaWrapper.consumer.commitMessage(data)
+        }
+    } catch (err) {
+        console.error(err)
+        // add error response to redis
+        KafkaWrapper.consumer.commitMessage(data)
+    }
+});
+
+// function createOrderDocument(payload, callback) {
+//     let order = {
+//         orderId: MUUID.from(payload.orderId),
+//         userId: MUUID.from(payload.userId),
+//         kitchenId: MUUID.from(payload.kitchenId),
+//         status: "orderCreated",
+//         totalPrice: payload.totalPrice
+//     }
+//     let newOrder = new Order(order)
+//     newOrder.save(err => callback(err))
+// }
+
+function setOrderStatusTo(orderId, status, callback) {
+    let filter = { orderId: MUUID.from(orderId) }
+    let update = { status }
+
+    Order.findOneAndUpdate(filter, update, {new: true}, (err, doc) => {
+        callback(err, doc)
+    })
+}
+
+KafkaWrapper.producer.on('ready', () => {
+    console.log('The producer has connected.')
+    KafkaWrapper.consumer.connect()
+})
