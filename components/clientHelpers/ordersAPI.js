b4bf4ee06fdeb449865720c3043903be8c002ca4
Anthony Amanse <ghieamanse@gmail.com>
2021-03-04 21:44:09 -0800

    Integrate order list from server
    Signed-off-by: Anthony Amanse <ghieamanse@gmail.com>

--- components/clientHelpers/ordersAPI.js
+++ components/clientHelpers/ordersAPI.js
@@ -27,3 +27,26 @@ async function createOrder(order) {
         }
     }
 }
+
+async function getOrdersOfUser(userId) {
+    if (STATIC_DATA) {
+        console.log(STATIC_DATA)
+        return new Promise(resolve => {
+            resolve("noop")
+        })
+    } else {
+        let invocationRequest = await fetch(API_URL.concat(`/user/${userId}/orders`))
+        let invocationResponse = await invocationRequest.json()
+        let invocationId = invocationResponse.requestId
+        let count = 10 // 10 tries
+        while (count > 0) {
+            let inlineDelay = await new Promise(resolve => setTimeout(resolve, 250)) // 250ms
+            statusRequest = await fetch(API_URL.concat("/status/").concat(invocationId))
+            statusJson = await statusRequest.json()
+            if (statusJson.status != "processing") {
+                return statusJson
+            }
+            count--
+        }
+    }
+}
--- components/receipt.js
+++ components/receipt.js
@@ -19,27 +19,57 @@ class Receipt extends HTMLElement {
         let res = await fetch('./components/receipt.html')
         var sr = this.shadowRoot;
         sr.innerHTML = await res.text();
+        // show stored locally then update
         this.showOrders();
+        this.updateLocalOrdersFromServer();
     }
 
     showOrders() {
         var orders = localStorage.getItem('KAFKA-ORDERS');
 
         var orderlist = JSON.parse(orders);
+        this.renderOrders(orderlist)
+    }
 
+    renderOrders(orderlist) {
         var sr = this.shadowRoot;
 
         var anchor = sr.getElementById('orderList');
 
-        orderlist.forEach(element => {
-            console.log(element);
-            var order = document.createElement('order-element');
-            order.setAttribute('dish', element.dish);
-            order.setAttribute('cost', element.cost);
-            order.setAttribute('type', element.type);
-            order.setAttribute('status', element.status);
-            anchor.appendChild(order);
-        });
+        // remove currently rendered orders
+        while (anchor.firstChild) {
+            anchor.removeChild(anchor.firstChild);
+        }
+
+        try {
+            orderlist.forEach(element => {
+                console.log(element);
+                var order = document.createElement('order-element');
+                order.setAttribute('dish', element.dish);
+                order.setAttribute('cost', element.totalPrice);
+                order.setAttribute('type', element.type);
+                order.setAttribute('status', element.status);
+                anchor.appendChild(order);
+            });
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    async updateLocalOrdersFromServer() {
+        // check for orders on server
+        let userId = localStorage.getItem("userId")
+        let ordersResponse = await getOrdersOfUser(userId)
+
+        if (ordersResponse != "noop") {
+            if (ordersResponse.status == "success") {
+                let orderlist = ordersResponse.docs
+                // clear and store current orders from server
+                localStorage.removeItem('KAFKA-ORDERS')
+                localStorage.setItem('KAFKA-ORDERS', JSON.stringify(orderlist))
+                this.renderOrders(orderlist)
+            }
+        }
     }
 }
 
